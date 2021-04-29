894ae5cf0062909b4cde14a03eaed0f5828c0fb0
Miles Martin <miles.martin@maine.edu>
2021-04-28 23:33:37 -0400

    Cleaned up captureSerial

--- captureSerial.py
+++ captureSerial.py
@@ -12,30 +12,27 @@ import time
 
 with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
 
-	print('Writing...')
-	ser.write(b'o')		# write a line to return a line
-	print('Written. Reading...')
-	time.sleep(1)
-	ser_bytes = ser.readline()	# read a line
-	print('Read. Printing...')
-	print(ser_bytes)		# print what was read
-	time.sleep(2)			# repeat in two seconds
+	ser.write(b'o')					# write a line to return a line
+	ser_bytes = ser.readline()			# read a line
+	print(ser_bytes.decode("utf-8","strict"))	# print what was read
+	time.sleep(1)					# repeat in two seconds
 
-import serial, csv
-import time
-import board
-import adafruit_ahtx0
+#import serial, csv
+#import time
+#import board
+#import adafruit_ahtx0
 
-filename = "testLog.csv"
+#filename = "testLog.csv"
 
-while True:
-	try:
-		with open(f"{filename}","a") as myfile:
-			writer = csv.writer(myfile,delimiter=",")
-			newrow = [time.time(),"%0.1f" % sensor.temperature,"%0.1f" % sensor.relative_humidity,0,0]
-			writer.writerow(newrow)
-			print(newrow)
-	except:
-		print("KeyboardInterrupt\n")
-		break
-	time.sleep(1)
+#while True:
+#	try:
+#		with open(f"{filename}","a") as myfile:
+#			writer = csv.writer(myfile,delimiter=",")
+#			newrow = [time.time(),"%0.1f" % sensor.temperature,"%0.1f" % sensor.relative_humidity,0,0]
+#			writer.writerow(newrow)
+#			print(newrow)
+#	except:
+#		print("KeyboardInterrupt\n")
+#		break
+#	time.sleep(1)
+
