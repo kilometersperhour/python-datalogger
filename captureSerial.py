d0a35b9a0fb1d6abad5d113144ea1f70db1c467a
Miles Martin <miles.martin@maine.edu>
2021-04-28 23:41:34 -0400

    captureSerial seems complete. The strings are printed in the correct format

--- captureSerial.py
+++ captureSerial.py
@@ -2,37 +2,18 @@
 
 # Miles Martin - ECE 331
 # Project 3 - Datalogger
-# Program: logDataToCSV.py
-# Purpose: Collect data from four sensors and save as CSV file for later storage in database
-
-#TODO: Still isn't reading anything. Fix that.
+# Program: captureSerial.py
+# Purpose: Collect data from serial-port-based sensor and print to screen
 
 import serial
 import time
+import string
 
 with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
 
-	ser.write(b'o')					# write a line to return a line
-	ser_bytes = ser.readline()			# read a line
-	print(ser_bytes.decode("utf-8","strict"))	# print what was read
-	time.sleep(1)					# repeat in two seconds
-
-#import serial, csv
-#import time
-#import board
-#import adafruit_ahtx0
-
-#filename = "testLog.csv"
-
-#while True:
-#	try:
-#		with open(f"{filename}","a") as myfile:
-#			writer = csv.writer(myfile,delimiter=",")
-#			newrow = [time.time(),"%0.1f" % sensor.temperature,"%0.1f" % sensor.relative_humidity,0,0]
-#			writer.writerow(newrow)
-#			print(newrow)
-#	except:
-#		print("KeyboardInterrupt\n")
-#		break
-#	time.sleep(1)
+	while True:
+		ser.write(b'o')						# write a line to return a line
+		ser_bytes = ser.readline()				# read a line
+		print(ser_bytes.decode("utf-8","strict").rstrip())	# pretty-print what was read
+		time.sleep(1)						# repeat in one second
 
