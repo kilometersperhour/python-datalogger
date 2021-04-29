8e1d34e0752ffdf4d456e7bbd648742a3c1f4e7d
Miles Martin <miles.martin@maine.edu>
2021-04-29 00:34:04 -0400

    using sample code from wiringpi-python github for an ISR template

--- captureGPIO.py
+++ captureGPIO.py
@@ -1,1 +1,16 @@
+# from https://github.com/WiringPi/WiringPi-Python/blob/master/examples/callback.py
+
 import wiringpi
+PIN_TO_SENSE = 23
+
+def gpio_callback():
+    print "GPIO_CALLBACK!"
+
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(PIN_TO_SENSE, wiringpi.GPIO.INPUT)
+    wiringpi.pullUpDnControl(PIN_TO_SENSE, wiringpi.GPIO.PUD_UP)
+
+    wiringpi.wiringPiISR(PIN_TO_SENSE, wiringpi.GPIO.INT_EDGE_BOTH, gpio_callback)
+
+    while True:
+        wiringpi.delay(2000)
