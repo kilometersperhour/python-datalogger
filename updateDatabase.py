e6b456b91d911c0d0be4d0b68aaa75bf7d219b19
Miles Martin <miles.martin@maine.edu>
2021-04-29 09:47:33 -0400

    Final version for updateDatabase.py if I can help it

--- updateDatabase.py
+++ updateDatabase.py
@@ -8,16 +8,10 @@ import os, shutil
 conn = sqlite3.connect('my.db')  
 c = conn.cursor()
 
-data_csv = r'./testLog.csv'
+data_csv = r'./data.csv'
 template_path = r'./legend.txt'
 read_clients = pd.read_csv (data_csv)
 read_clients.to_sql('Datalogger', conn, if_exists='append', index = False) # Insert the values from the csv file into the table 'Datalogger' 
 
-os.remove(data_csv)
-# template_fd = os.open(template_path, os.O_RDONLY)
-# file_size = os.path.getsize(template_path)
-# duplicate_fd = os.open(data_csv, os.O_RDWR | os.O_CREAT)
-# os.sendfile(template_fd,duplicate_fd, 0, file_size) # offset is 0
-shutil.copy2(template_path,data_csv)
-# print(os.read(template_fd,os.path.getsize(template_path)))
-# print(template_path,os.path.getsize(template_path))
+os.remove(data_csv) # delete previously input data
+shutil.copy2(template_path,data_csv) # make new csv for storage with same headers as previous
