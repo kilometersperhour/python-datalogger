b84596d43b6c7c6be1d3d08773cd3b98133b5f13
Miles Martin <miles.martin@maine.edu>
2021-04-29 10:06:55 -0400

    More progress on drawing plots. Some stuff for connecting to db

--- drawNewPlot.py
+++ drawNewPlot.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+# Miles Martin - ECE 331
+# Project 3 - Datalogger
+# Program: makeNewPlot.py
+# Purpose: Draw new plots for the website
+
+import sqlite3, time, datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+conn = sqlite3.connect('my.db')
+c = conn.cursor()
+
+columns = ('Unix_Time','Temperature','Relative_Humidity','Ambient_Light','PIR_Voltage')
