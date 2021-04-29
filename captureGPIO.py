82cff7051d747121a3a347f201d1f44069451504
Miles Martin <miles.martin@maine.edu>
2021-04-29 01:34:41 -0400

    All captureGPIO needs is a nice header and it should be good to add to the logger script

--- captureGPIO.py
+++ captureGPIO.py
@@ -1,16 +1,20 @@
-# from https://github.com/WiringPi/WiringPi-Python/blob/master/examples/callback.py
+#!/usr/bin/python3
 
 import wiringpi
-PIN_TO_SENSE = 23
+PIN_TO_SENSE = 17
 
-def gpio_callback():
-    print "GPIO_CALLBACK!"
+def gpio_edge():
+	if wiringpi.digitalRead(PIN_TO_SENSE):
+		voltage = "HIGH"
+	else:
+		voltage = "LOW"
+	print(f"Edge occurred. Voltage is {voltage}.")
 
-    wiringpi.wiringPiSetupGpio()
-    wiringpi.pinMode(PIN_TO_SENSE, wiringpi.GPIO.INPUT)
-    wiringpi.pullUpDnControl(PIN_TO_SENSE, wiringpi.GPIO.PUD_UP)
+wiringpi.wiringPiSetupGpio() # initialize pins with wiringPi numbering
+wiringpi.pinMode(PIN_TO_SENSE, wiringpi.GPIO.INPUT) # wiringPi 0 (GPIO 17) is an input
+wiringpi.pullUpDnControl(PIN_TO_SENSE, wiringpi.GPIO.PUD_UP) # set pull up resistor
 
-    wiringpi.wiringPiISR(PIN_TO_SENSE, wiringpi.GPIO.INT_EDGE_BOTH, gpio_callback)
+wiringpi.wiringPiISR(PIN_TO_SENSE, wiringpi.GPIO.INT_EDGE_BOTH, gpio_edge)
 
-    while True:
-        wiringpi.delay(2000)
+while True:
+	wiringpi.delay(2000)
